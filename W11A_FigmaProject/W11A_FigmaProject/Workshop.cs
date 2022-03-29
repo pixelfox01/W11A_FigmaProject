318a1cec58a3fe12aa9b7b344833db9778f62dd8
Imran <imranyafith@gmail.com>
2022-03-29 18:37:01 -0400

    Created Workshop class

---
+++ W11A_FigmaProject/W11A_FigmaProject/Workshop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W11A_FigmaProject
+{
+    class Workshop
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public List<String> Details { get; set; }
+    }
+}
