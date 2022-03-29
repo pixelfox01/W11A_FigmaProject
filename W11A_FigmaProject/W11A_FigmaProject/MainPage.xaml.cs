1437e481ec8d6e35b4bdcb0c2d1bf71bb35509fe
Imran <imranyafith@gmail.com>
2022-03-29 18:17:38 -0400

    Initial commit

---
+++ W11A_FigmaProject/W11A_FigmaProject/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace W11A_FigmaProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
